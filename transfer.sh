be3efa6a9ee7840f1b219a0950d4807228f5adf6
fan <fwf@ddpkcc.com>
2020-06-01 19:50:11 +0800

    添加说明

--- transfer.sh
+++ transfer.sh
@@ -22,14 +22,14 @@
 ###
  # @Author: 范文富
  # @Date: 2020-03-12 17:34:15
- # @LastEditTime: 2020-06-01 18:57:27
+ # @LastEditTime: 2020-06-01 19:49:53
  # @LastEditors: Please set LastEditors
  # @Description: 构建部署脚本
  # @FilePath: /next-antd-node-blog/transfer.sh
 ### 
 
 # /bin/bash
-
+# ssh fanblog 命令需要配置ssh免密登陆 配置教程 https://blog.csdn.net/u013239054/article/details/84027557
 cd 'src'
 yarn build
 echo "构建成功"
